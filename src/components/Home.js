61f0f91f9c222e7847e82185f2b0beb03147605a
Girish <girishfriends.2012@gmail.com>
2018-04-05 22:28:03 +0530

    saving for myself

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,51 +17,48 @@ export default class Home extends Component {
         teamNames
       })))
   }
-  
+
   redirect() {
     this.setState({redirect: true});
-  } 
-  
+  }
+
+  onCardClick() {
+    this.setState({cardClicked: true});
+  }
   render() {
+    if(this.state.cardClicked) {
+      return <Redirect push to="/jobcard/1" />
+    }
     if (this.state.redirect) {
     return <Redirect push to="/jobcard/0" />;
    }
     const { teamNames } = this.state;
-    
+
     const pStyle = {
       float: 'right'
     };
 
+    const extra = (
+      <a>
+       <Icon name='indent' />
+       2 Indents
+      </a>
+    )
     return (
       <div className='container'>
-      
+
         <div style={pStyle}>
           <Button primary size="large" onClick={() => this.redirect()}>
           Create Job Card </Button>
         </div>
-      <Card>
-      <Image src={require('../background.jpg')} size='large'/>
-       <Card.Content>
-         <Card.Header>
-           Matthew
-         </Card.Header>
-         <Card.Meta>
-           <span className='date'>
-             Joined in 2015
-           </span>
-         </Card.Meta>
-         <Card.Description>
-           Matthew is a musician living in Nashville.
-         </Card.Description>
-       </Card.Content>
-       <Card.Content extra>
-         <a>
-           <Icon name='user' />
-           22 Friends
-         </a>
-       </Card.Content>
- </Card>
-      
+      <Card onClick={() => this.onCardClick()}
+      header='KA 51 RK 1234'
+      meta='Friend'
+      description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+      extra={extra}
+      />
+
+
       </div>
     )
   }
