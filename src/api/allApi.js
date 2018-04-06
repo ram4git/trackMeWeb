4e4f4ff5b96e7af27758324c73a9503ab71bd1ba
Rakesh <rrampall@cisco.com>
2018-04-06 19:11:45 +0530

    Saving job card

---
+++ src/api/allApi.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+import FireBaseTools from './firebase-tools'
+
+
+export function getStoreCases() {
+  const dbRef = firebase.database().ref().child('roles/store/openCases');
+  return dbRef.once('value');
+}
+
+
+export function createJobCard(data) {
+  const dbRef = firebase.database().ref().child('jobCards');
+  const updates = {};
+  updates[`${data.id}`] = data;
+  return dbRef.update(updates);
+}
--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react'
 import { Redirect, Link } from 'react-router-dom'
-import { getTeamNames, getTeamsArticles } from '../api'
 import TeamLogo from './TeamLogo'
 import Team from './Team'
 import slug from 'slug'
 import Loading from './Loading'
 import { Input, Label, Form , Select,Header, Image,Dropdown,
-  Grid, Table, Button} from 'semantic-ui-react'
+  Grid, Table, Button, TextArea } from 'semantic-ui-react'
+
+import {createJobCard} from '../api/allApi.js'
 
 
 export default class JobCard extends Component {
   state = {
-    loading: true,
-    teamNames: {},
-    articles: [],
+    loading: true
   }
 
 
@@ -31,9 +30,20 @@ export default class JobCard extends Component {
     console.log(this.props.match.params.id)
   }
 
-  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+  handleChange = (e, {name , value}) =>{
+    console.log(value);
+      this.setState({ [name]: value } , ()=>{console.log(this.state)});
+  }
 
- handleSubmit = () => this.setState({ email: '', name: '' })
+ handleSubmit = () => {
+   console.log(this.state);
+   let data = Object.assign({}, this.state);
+  // let copy = {{} , ...this.state};
+   data.date = new Date();
+   data.id = '123'
+   createJobCard(data).then(console.log('successfully saved')).catch(console.log('error occured'))
+   this.setState({ email: '', name: '' })
+ }
 
 
 
@@ -48,6 +58,19 @@ export default class JobCard extends Component {
       marginTop: '40px'
     };
 
+    const durationOptions = [
+      { key: '1', text: '1 day', value: '1' },
+      { key: '2', text: '2 days', value: '2' },
+      { key: '3', text: '3 days', value: '3' },
+      { key: '4', text: '4 days', value: '4' },
+      { key: '5', text: '5 days', value: '5' },
+      { key: '6', text: '6 days', value: '6' },
+      { key: '7', text: '1 week', value: '7' },
+      { key: '8', text: '1.5 week', value: '10' },
+      { key: '9', text: '2 weeks', value: '14' },
+      { key: '10', text: '3 weeks', value: '21' },
+      { key: '11', text: '1 month', value: '30' }
+    ];
     const options = [
   { key: 'angular', text: 'Angular', value: 'angular' },
   { key: 'css', text: 'CSS', value: 'css' },
@@ -86,7 +109,7 @@ export default class JobCard extends Component {
           label={<Label color="teal" size="large">Vehicle Number</Label>}
            >
           <Input size="large"
-          placeholder='Search...'
+          placeholder='Search...' name="vehicleNumber" onChange={this.handleChange}
           icon={{ name: 'search', circular: true, link: true }}
           />
           </Form.Field>
@@ -158,44 +181,44 @@ export default class JobCard extends Component {
     </Grid>
     <Form.Group style={mStyle} widths={3}>
          <Form.Field control={Dropdown} fluid label='Complaint Type'
-             placeholder='Skills' fluid multiple selection options={options} >
+             placeholder='Skills' fluid multiple selection options={options} name="complaintType"  onChange={this.handleChange}>
           </Form.Field>
-         <Form.Select fluid label='Category' options={options} placeholder='Gender' />
-         <Form.Select fluid label='Checked by' options={options} placeholder='Gender' />
+         <Form.Select fluid label='Category' options={options}  name="complaintCategory" placeholder='Gender' onChange={this.handleChange} />
+         <Form.Select fluid label='Checked by' options={options}  name="checkedBy" placeholder='Gender' onChange={this.handleChange} />
 
    </Form.Group>
+   <Form.Group style={mStyle} widths={3}>
+   <Form.Field width={3} control={Input} label='Previous Meter Reading' name="previousMeterReading" placeholder='3400'
+   onChange={this.handleChange} >
+    </Form.Field>
+    <Form.Field width={2} control={Input} label='Preset Reading' name="presetReading" placeholder='4300'
+    onChange={this.handleChange} >
+     </Form.Field><Form.Field width={2} control={Input} label='ODOMeter Reading(KM)' name="odometerReading" placeholder='12000'
+     onChange={this.handleChange} >
+      </Form.Field>
+
+  </Form.Group>
    <Form.Group style={mStyle} widths={2}>
-        <Form.Field width={2} control={Input} label='Estimated Duration to complete' placeholder='2 days' >
-         </Form.Field>
-         <Form.Select fluid label='Mechanic Alloted' options={options} placeholder='Raju' />
+          <Dropdown
+           button
+           width={3}
+           className='icon'
+           floating
+           labeled
+           icon='time'
+           name="duration"
+           options={durationOptions}
+           defaultValue="1"
+           search
+           selection
+           onChange={this.handleChange}
+         />
+
+         <Form.Select fluid label='Mechanic Alloted' name="mechanicAllotted" options={options} placeholder='Raju' onChange={this.handleChange}/>
 
 
   </Form.Group>
-  <Form.Field label='Complaint description' control='textarea' rows='3' width={6}/>
-
-
-
-          {/* <Form.Field width="6">
-           <Label icon="bus"  size="large">Vehicle Number</Label>
-           <Input size="large"
-           placeholder='Search...'
-           icon={{ name: 'search', circular: true, link: true }}
-           />
-           </Form.Field>
-
-           <Form.Group widths='equal'>
-             <Form.Input fluid label='Job card number' placeholder='JB340' readOnly />
-             <Form.Input fluid label='Created by' placeholder='Krishna Kumar' readOnly />
-             <Form.Input fluid label='Created at' placeholder='Read only' readOnly />
-           </Form.Group>
-           <Form.Group widths='equal'>
-             <Form.Input label='Make' placeholder='Ashok Leyland' readOnly />
-             <Form.Input fluid label='Number of tyres' placeholder='4' readOnly />
-             <Form.Input fluid label='Driver Name' placeholder='Ram Kumar' readOnly />
-           </Form.Group>
-
-
-            */}
+  <Form.Field label='Complaint description' control={TextArea} name="description" rows='3' width={6} onChange={this.handleChange}/>
             <Form.Field control={Button} color='green' content='SUBMIT' width={3} />
        </Form>
       </div>
