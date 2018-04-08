a14b0d0009a26299afa70afab1d1e7b3cbb471e5
Girish <girishfriends.2012@gmail.com>
2018-04-08 21:29:13 +0530

    jobcard Details Table

--- src/components/ViewJobCard.js
+++ src/components/ViewJobCard.js
@@ -4,9 +4,8 @@ import TeamLogo from './TeamLogo'
 import Team from './Team'
 import slug from 'slug'
 import Loading from './Loading'
-import { Input, Label, Form , Select,Header, Image,Dropdown,
-  Grid, Table, Button, TextArea } from 'semantic-ui-react'
-
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
 import { getJobCardDetail } from '../api/allApi.js'
 
 export default class ViewJobCard extends Component {
@@ -21,9 +20,7 @@ export default class ViewJobCard extends Component {
     const { jobCardID } = this.props;
     let jobCard;
     if(window.localStorage.jobCards) {
-
     let jobCards = JSON.parse(window.localStorage.jobCards);
-
     let jobCard =  jobCards[jobCardID];
 }
     if(!jobCard){
@@ -41,12 +38,73 @@ export default class ViewJobCard extends Component {
   }
 
   render() {
-    const { jobCard } = this.state;
+  const { jobCard } = this.state;
   if(!jobCard) {
    return null;
  }
-    return <h1>{jobCard.checkedBy}</h1>
+    return (
+      <Paper>
+      <Table>
+        <TableBody>
+           <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>{jobCard.id}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Vehicle Number</TableCell>
+            <TableCell>{jobCard.vehicleNumber}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Checked By</TableCell>
+            <TableCell>{jobCard.checkedBy}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Complaint Category</TableCell>
+            <TableCell>{jobCard.complaintCategory}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Complaint Type</TableCell>
+            <TableCell>{jobCard.complaintType}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell>{jobCard.date}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Description</TableCell>
+            <TableCell>{jobCard.description}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Duration</TableCell>
+            <TableCell>{jobCard.duration}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Indents</TableCell>
+            <TableCell>{jobCard.indents}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Loading</TableCell>
+            <TableCell>{jobCard.loading}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Mechanic Allotted</TableCell>
+            <TableCell>{jobCard.mechanicAllotted}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Odometer Reading</TableCell>
+            <TableCell>{jobCard.odometerReading}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Preset Reading</TableCell>
+            <TableCell>{jobCard.presetReading}</TableCell>
+           </TableRow>
+           <TableRow>
+            <TableCell>Previous Meter Reading</TableCell>
+            <TableCell>{jobCard.previousMeterReading}</TableCell>
+           </TableRow>
+        </TableBody>
+      </Table>
+    </Paper>
+    )
   }
-
-
 }
